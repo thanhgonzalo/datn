ca282073a6168e99b3f90767abb53d9211f65672
thanhnd <thanhnd.bkhn@gmail.com>
2018-12-20 20:49:28 +0700

    commit

--- shop-tmdt/app/Http/Controllers/OnePayController.php
+++ shop-tmdt/app/Http/Controllers/OnePayController.php
@@ -10,16 +10,40 @@ namespace App\Http\Controllers;
 
 
 
-use Illuminate\Support\Facades\Config;
+use App\Http\Service\ServiceOnePay;
 
 class OnePayController extends Controller
 {
     public function postorder() {
-        $url = 'https://mtf.onepay.vn/onecomm-pay/vpc.op?Title=VPC+3-Party&vpc_AccessCode=D67342C2&vpc_Amount=100&vpc_Command=pay&vpc_Currency=VND&vpc_Customer_Email=support%40onepay.vn&vpc_Customer_Id=thanhvt&vpc_Customer_Phone=840904280949&vpc_Locale=vn&vpc_MerchTxnRef=20181219225448128483959&vpc_Merchant=ONEPAY&vpc_OrderInfo=JSECURETEST01&vpc_ReturnURL=http%3A%2F%2F127.0.0.1%2Fpaymentonepay&vpc_SHIP_City=Ha+Noi&vpc_SHIP_Country=Viet+Nam&vpc_SHIP_Provice=Hoan+Kiem&vpc_SHIP_Street01=39A+Ngo+Quyen&vpc_TicketNo=%3A%3A1&vpc_Version=2&vpc_SecureHash=2919A010AD6D62712C53EFEF87410D8C09C4B2E798B59EBB4977BE22749A9955';
+
+        $data = array(
+            'Title' => 'Test',
+            'AgainLink' => 'http://127.0.0.1/dat-hang?paymethod=onpay',
+            'vpc_Amount' => 100 * 100,
+            'vpc_Customer_Email' => 'thanhnd@gmail.com',
+            'vpc_Customer_Id' => 'thanhnd@gmail.com',
+            'vpc_Customer_Phone' => '0967211692',
+            'vpc_MerchTxnRef' => '20181219224614121363325s',
+            'vpc_OrderInfo' => 'Dat hang test',
+            'vpc_ReturnURL' => 'http://127.0.0.1/paymentonepay',
+            'vpc_SHIP_City' => 'Hà Nội',
+            'vpc_SHIP_Country' => 'Viet Nam',
+            'vpc_SHIP_Provice' => 'Cau Giay',
+            'vpc_SHIP_Street01' => '108 Hoang Quoc Viet',
+            'vpc_TicketNo' => '%3A%3A1',
+        );
+        $onePay = new ServiceOnePay($data);
+        $url = $onePay->buildUrl();
+//                $url = 'https://mtf.onepay.vn/onecomm-pay/vpc.op?Title=VPC+3-Party&vpc_AccessCode=D67342C2&vpc_Amount=100&vpc_Command=pay&vpc_Currency=VND&vpc_Customer_Email=support%40onepay.vn&vpc_Customer_Id=thanhvt&vpc_Customer_Phone=840904280949&vpc_Locale=vn&vpc_MerchTxnRef=20181219225448128483959&vpc_Merchant=ONEPAY&vpc_OrderInfo=JSECURETEST01&vpc_ReturnURL=http%3A%2F%2F127.0.0.1%2Fpaymentonepay&vpc_SHIP_City=Ha+Noi&vpc_SHIP_Country=Viet+Nam&vpc_SHIP_Provice=Hoan+Kiem&vpc_SHIP_Street01=39A+Ngo+Quyen&vpc_TicketNo=%3A%3A1&vpc_Version=2&vpc_SecureHash=2919A010AD6D62712C53EFEF87410D8C09C4B2E798B59EBB4977BE22749A9955';
+//
+//        $url = 'https://mtf.onepay.vn/onecomm-pay/vpc.op?Title=VPC+3-Party&vpc_AccessCode=D67342C2&vpc_Amount=100&vpc_Command=pay&vpc_Currency=VND&vpc_Customer_Email=support%40onepay.vn&vpc_Customer_Id=thanhvt&vpc_Customer_Phone=840904280949&vpc_Locale=vn&vpc_MerchTxnRef=20181219225448128483959&vpc_Merchant=ONEPAY&vpc_OrderInfo=JSECURETEST01&vpc_ReturnURL=http%3A%2F%2F127.0.0.1%2Fpaymentonepay&vpc_SHIP_City=Ha+Noi&vpc_SHIP_Country=Viet+Nam&vpc_SHIP_Provice=Hoan+Kiem&vpc_SHIP_Street01=39A+Ngo+Quyen&vpc_TicketNo=%3A%3A1&vpc_Version=2&vpc_SecureHash=2919A010AD6D62712C53EFEF87410D8C09C4B2E798B59EBB4977BE22749A9955';
         return redirect()->to($url)->send();
     }
 
     public function getorder() {
-        $config = Config::get('onepay');
+        var_dump('tvpc_ReturnURL
+        
+        
+        tt'); exit;
     }
 }
--- shop-tmdt/config/onepay.php
+++ shop-tmdt/config/onepay.php
@@ -1,26 +1,15 @@
 <?php
-return array(
-    'onepay_common' => array(
-        'url_Onepay'      => 'https://mtf.onepay.vn/onecomm-pay/vpc.op',
-        'vpc_Version'     => 2,
-        'vpc_Currency'    => 'VND',
-        'vpc_Command'     => 'pay',
-        'vpc_AccessCode'  => 'A3EFDFABA8653DF2342E8DAC29B51AF0',
-        'vpc_Merchant'    => 'ONEPAY',
-        'vpc_Locale'      => 'vn',
-        'vpc_ReturnURL'   => 'http%3A%2F%2127.0.0.1%F',
-        'vpc_MerchTxnRef' => '20181219224614121363414',
-        'vpc_OrderInfo'   => 'Đơn hàng gửi từ ThanhND',
-        'vpc_Amount'      => '10000',
-        'vpc_TicketNo'    => '%3A%3A1'
-    ),
-    'onepay_customer' => array(
-        'vpc_SHIP_Street01'  => 'Hải Dương',
-        'vpc_SHIP_Provice'   => 'Thanh Hà',
-        'vpc_SHIP_City'      => 'Thành Phố',
-        'vpc_Customer_Phone' => '0967211692',
-        'vpc_Customer_Email' => 'thanhnd@gmail.com',
-        'vpc_Customer_Id'    => '3'
-    ),
-);
-
+return [
+    'domestic' => [ // Noi dia
+        'vpc_Merchant' => 'ONEPAY',
+        'vpc_AccessCode' => 'D67342C2',
+        'vpc_SecureHash' => 'A3EFDFABA8653DF2342E8DAC29B51AF0',
+        'vpc_url' => 'https://mtf.onepay.vn/onecomm-pay/vpc.op',
+    ],
+    'international' => [ // Quoc te
+        'vpc_Merchant' => 'TESTONEPAY',
+        'vpc_AccessCode' => '6BEB2546',
+        'vpc_SecureHash' => '6D0870CDE5F24F34F3915FB0045120DB',
+        'vpc_url' => 'https://mtf.onepay.vn/vpcpay/vpcpay.op',
+    ],
+];
